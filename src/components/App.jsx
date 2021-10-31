dc9a25e642ef336c12455fa6ebfb8a2f30a857e8
Alex Glau <glaualex89@gmail.com>
2021-10-31 21:35:15 +0500

    Base routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import Start from './pages/Start';
+import Test from './pages/Test';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <Start />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/test" exact>
+          <Test />
+        </Route>
+        <Route path="/" exact>
+          <Start />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
