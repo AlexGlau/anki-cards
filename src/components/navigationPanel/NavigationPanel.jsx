f6b2e0c8e1e9a8f366912d143b6bc5a59b806e58
Alex Glau <glaualex89@gmail.com>
2021-11-07 20:05:50 +0500

    Updated navigation panel

--- src/components/navigationPanel/NavigationPanel.jsx
+++ src/components/navigationPanel/NavigationPanel.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavigationPanel = () => {
   return (
     <div className="nav-panel">
-      <NavLink className="nav-tab">Home</NavLink>
-      <NavLink className="nav-tab">Test</NavLink>
-      <NavLink className="nav-tab">New words</NavLink>
+      <NavLink to="/" className="nav-tab" exact>Home</NavLink>
+      <NavLink to="/new" className="nav-tab">New words</NavLink>
+      <NavLink to="/test" className="nav-tab">Test</NavLink>
     </div>
   );
 };
