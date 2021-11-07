4f1ad92a6ed21a0366b0f2c5fcea6069f0edf7ed
Alex Glau <glaualex89@gmail.com>
2021-11-07 20:06:33 +0500

    Updated start page

--- src/components/pages/Start.jsx
+++ src/components/pages/Start.jsx
@@ -7,17 +7,15 @@ import './Start.scss';
 const Start = () => {
   return (
     <div className="start container">
-      <div className="lists">
-        <Button className="list-button btn btn-light">List of words</Button>
-        <Button className="list-button btn btn-light">List of words</Button>
-        <Button className="list-button btn btn-light">List of words</Button>
-        <Button className="list-button btn btn-light">List of words</Button>
-        <Button className="list-button btn btn-light">List of words</Button>
-        <Button className="list-button btn btn-light">List of words</Button>
-      </div>
-      <div className="buttons">
-        <Button className="new-list-button btn btn-primary">New list</Button>
-        <Button className="menu-button btn btn-primary">Menu</Button>
+      <div className="row">
+        <div className="lists col">
+          <Button className="list-button btn btn-light">List of words</Button>
+          <Button className="list-button btn btn-light">List of words</Button>
+          <Button className="list-button btn btn-light">List of words</Button>
+          <Button className="list-button btn btn-light">List of words</Button>
+          <Button className="list-button btn btn-light">List of words</Button>
+          <Button className="list-button btn btn-light">List of words</Button>
+        </div>
       </div>
     </div>
   );
