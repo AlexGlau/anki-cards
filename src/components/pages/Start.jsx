da61597991e6472142923b89ec3c5ca365731f3f
Alex Glau <glaualex89@gmail.com>
2023-02-12 14:30:09 +0400

    Show added words

--- src/components/pages/Start.jsx
+++ src/components/pages/Start.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Button from '../controls/Button';
 
 import './Start.scss';
 
 const Start = () => {
+  const wordsList = useSelector((state) => {
+    return state.words;
+  });
+
   return (
     <div className="start container">
       <div className="row">
         <div className="lists col">
-          <Button className="list-button btn btn-light">List of words</Button>
-          <Button className="list-button btn btn-light">List of words</Button>
-          <Button className="list-button btn btn-light">List of words</Button>
-          <Button className="list-button btn btn-light">List of words</Button>
-          <Button className="list-button btn btn-light">List of words</Button>
-          <Button className="list-button btn btn-light">List of words</Button>
+          {wordsList.map((word, index) => {
+            return (
+              <Button
+                key={`${index}-${word.word}`}
+                className="list-button btn btn-light"
+              >
+                {word.word}
+              </Button>
+            );
+          })}
         </div>
       </div>
     </div>
