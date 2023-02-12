6254b085e974fbf9b2d86cb52e59af52d59b66e6
Alex Glau <glaualex89@gmail.com>
2023-02-12 14:06:24 +0400

    Add translation

--- src/components/pages/New.jsx
+++ src/components/pages/New.jsx
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addWord } from '../../store';
+
+import Input from '../controls/Input';
+import Button from '../controls/Button';
 
 const New = () => {
+  const [ word, setWord ] = useState('');
+  const [ translation, setTranslation ] = useState('');
+  const dispatch = useDispatch();
+
+  const addWordHandler = () => {
+    const action = addWord({ word, translation });
+    dispatch(action);
+  };
+
+  const setWordHandler = (e) => {
+    setWord(e.target.value);
+  };
+
+  const setTranslationHandler = (e) => {
+    setTranslation(e.target.value);
+  };
+
   return (
-    <div className="new"></div>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <Input
+            id="word"
+            className={''}
+            inputName="Word"
+            onChange={setWordHandler}
+            value={word}
+          />
+        </div>
+        <div className="col-12">
+          <Input
+            id="translate"
+            className={''}
+            inputName="Translation"
+            onChange={setTranslationHandler}
+            value={translation}
+          />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <Button
+            className="btn btn-primary"
+            onClick={addWordHandler}
+          >
+            Add
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
